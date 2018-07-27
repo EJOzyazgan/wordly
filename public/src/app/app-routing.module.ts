b8fa5ce548a46de7dbf3f204977b72118945afa7
EJ Ozyazgan <ejozyazgan@gmail.com>
2018-07-27 05:35:12 -0700

    Feed CSS

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from "@angular/router";
 
 const appRoutes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'feed'},
+  {path: '', pathMatch: 'full', redirectTo: 'feed/mainFeed'},
   {path: 'auth', loadChildren: './auth/auth.module#AuthModule'},
   {path: 'feed', loadChildren: './feed/feed.module#FeedModule'},
 
